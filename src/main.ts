53f5b491f34ae3da12819d2f5300cd686fd72054
AdrianM16 <adriananunciacion80@gmail.com>
2026-03-19 22:33:02 +0800

    simplify main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { provideRouter, withRouterConfig } from '@angular/router';
-import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 import { authInterceptor } from './app/core/interceptors/auth.interceptor';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes, withRouterConfig({ onSameUrlNavigation: 'reload' })),
-    provideHttpClient(withInterceptors([authInterceptor])),
-    provideAnimations()
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([authInterceptor]))
   ]
 }).catch((err) => console.error(err));
